bc86621a7e3b643d65b06467d1391d3b6c612e0d
vovaf <vovafokyz@gmail.com>
2023-10-06 16:40:51 +0300

    Репозиторий под проект Tracker

---
+++ src/main/java/ru/job4j/oop/Calculator.java
@@ -0,0 +1,32 @@
+package ru.job4j.oop;
+
+public class Calculator {
+
+    private static int x = 5;
+
+    public static int sum(int y) {
+        return x + y;
+    }
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        int rsl = calculator.sumAllOperation(5);
+        System.out.println(rsl);
+    }
+
+    public int multiply(int a) {
+        return x * a;
+    }
+
+    public int minus(int a) {
+        return a - x;
+    }
+
+    public int divide(int a) {
+        return a / x;
+    }
+
+    public int sumAllOperation(int a) {
+        return sum(a) + minus(a) + divide(a) + multiply(a);
+    }
+}
