ca3432103b16274282c3e189691343cd7597ca56
vovaf <vovafokyz@gmail.com>
2023-10-06 10:25:32 +0300

    Репозиторий под проект Tracker

--- src/main/java/ru/job4j/checkstyle/Broken.java
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -2,24 +2,25 @@ package ru.job4j.checkstyle;
 
 public class Broken {
     private final int sizeOfEmpty = 10;
+
     private String name;
 
-    public String surname;
+    private String surname;
+
     public static String newValue = "";
 
-    Broken() {
+    public Broken() {
     }
 
-    void echo() {
+    public void echo() {
     }
 
-    void media(Object obj) {
+    public void media(Object obj) {
         if (obj != null) {
             System.out.println(obj);
         }
     }
 
-    void method(int a, int b, int c, int d, int e, int f, int g) {
-
+    public void method(int a, int b, int c, int d, int e, int f, int g) {
     }
 }
