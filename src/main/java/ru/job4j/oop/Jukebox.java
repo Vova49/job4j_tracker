550cd05c54e4bbf6bceea6727c7a6bb6d0a50180
vovaf <vovafokyz@gmail.com>
2023-10-07 12:24:15 +0300

    Репозиторий под проект Tracker

--- src/main/java/ru/job4j/oop/Jukebox.java
+++ src/main/java/ru/job4j/oop/Jukebox.java
@@ -2,16 +2,6 @@ package ru.job4j.oop;
 
 public class Jukebox {
     public static void music(int position) {
-        switch (position) {
-            case 1:
-                System.out.println("Пусть бегут неуклюже");
-                break;
-            case 2:
-                System.out.println("Спокойной ночи");
-                break;
-            default:
-                System.out.println("Песня не найдена");
-        }
         String result = switch (position) {
             case 1 -> "Пусть бегут неуклюже";
             case 2 -> "Спокойной ночи";
