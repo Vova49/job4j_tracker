cbf0871e9a0798319463794bbe5602e8ba45887b
vovaf <vovafokyz@gmail.com>
2023-10-21 10:49:52 +0300

    Репозиторий под проект Tracker

--- src/main/java/ru/job4j/io/MagicBall.java
+++ src/main/java/ru/job4j/io/MagicBall.java
@@ -7,18 +7,14 @@ public class MagicBall {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Я великий Оракул. Что ты хочешь узнать? ");
-        String name = input.nextLine();
+        String question = input.nextLine();
         int answer = new Random().nextInt(3);
-        switch (answer) {
-            case 0:
-                System.out.println("Да");
-                break;
-            case 1:
-                System.out.println("Нет");
-                break;
-            default:
-                System.out.println("Может быть");
-                break;
-        }
+        String response = switch (answer) {
+            case 0 -> "Да";
+            case 1 -> "Нет";
+            default -> "Может быть";
+        };
+        System.out.println(response);
     }
 }
+
