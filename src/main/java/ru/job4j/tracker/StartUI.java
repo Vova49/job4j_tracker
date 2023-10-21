8595cdd092f888fc168f4e9e104e600fc75f9163
vovaf <vovafokyz@gmail.com>
2023-10-21 13:17:22 +0300

    Репозиторий под проект Tracker

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -38,6 +38,15 @@ public class StartUI {
                 } else {
                     System.out.println("Ошибка замены заявки.");
                 }
+            } else if (select == 3) {
+                System.out.println("=== Удаление заявки ===");
+                System.out.print("Введите id: ");
+                int id = Integer.parseInt(scanner.nextLine());
+                if (tracker.delete(id)) {
+                    System.out.println("Заявка удалена успешно.");
+                } else {
+                    System.out.println("Ошибка удаления заявки. Возможно, заявка с таким ID не существует.");
+                }
             } else if (select == 6) {
                 run = false;
             }
