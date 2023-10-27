13575d316af5abc4a2615445623a744e92ac90be
vovaf <vovafokyz@gmail.com>
2023-10-27 12:04:41 +0300

    Репозиторий под проект Tracker

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,52 +1,5 @@
 package ru.job4j.tracker;
 
-import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
 class StartUITest {
-    @Test
-    void whenCreateItem() {
-        Input in = new StubInput(
-                new String[]{"0", "Item name", "1"}
-        );
-        Tracker tracker = new Tracker();
-        UserAction[] actions = {
-                new CreateAction(),
-                new ExitAction()
-        };
-        new StartUI().init(in, tracker, actions);
-        assertThat(tracker.findAll()[0].getName()).isEqualTo("Item name");
-    }
 
-    @Test
-    void whenReplaceItem() {
-        Tracker tracker = new Tracker();
-        Item item = tracker.add(new Item("Replaced item")); /* Добавляется в tracker новая заявка */
-        String replacedName = "New item name";
-        Input in = new StubInput(
-                new String[]{"0", String.valueOf(item.getId()), "New item name", "1"}
-        );
-        UserAction[] actions = {
-                new ReplaceAction(),
-                new ExitAction()
-        };
-        new StartUI().init(in, tracker, actions);
-        assertThat(tracker.findById(item.getId()).getName()).isEqualTo(replacedName);
-    }
-
-    @Test
-    void whenDeleteItem() {
-        Tracker tracker = new Tracker();
-        Item item = tracker.add(new Item("Deleted item")); /* Добавляется в tracker новая заявка */
-        Input in = new StubInput(
-                new String[]{"0", String.valueOf(item.getId()), "1"}
-        );
-        UserAction[] actions = {
-                new DeleteAction(),
-                new ExitAction()
-        };
-        new StartUI().init(in, tracker, actions);
-        assertThat(tracker.findById(item.getId())).isNull();
-    }
 }
