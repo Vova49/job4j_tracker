7fc76a7804aac70c351eff6a7673f2a886931e08
vovaf <vovafokyz@gmail.com>
2023-10-28 19:05:14 +0300

    Репозиторий под проект Tracker

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,28 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for (int i = 0; i < value.length; i++) {
+            if (value[i] == key) {
+                rsl = i;
+                break;
+            }
+        }
+        if (rsl == -1) {
+            throw new IllegalArgumentException("ElementNotFoundException");
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        try {
+            int index = indexOf(new String[]{"a", "b", "c"}, "b");
+            System.out.println("Element found at index: " + index);
+            index = indexOf(new String[]{"a", "b", "c"}, "g");
+            System.out.println("Element found at index: " + index);
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
