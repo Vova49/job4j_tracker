00e5ada7c9ca039cb3993835ce72c761e61e6490
vovaf <vovafokyz@gmail.com>
2023-11-01 08:19:05 +0200

    Репозиторий под проект Tracker

--- src/main/java/ru/job4j/early/PasswordValidator.java
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -26,7 +26,9 @@ public class PasswordValidator {
                 hasDigit = true;
             } else if (!Character.isLetterOrDigit(symbol)) {
                 hasSpecial = true;
+                break;
             }
+
         }
 
         if (!hasUpCase) {
