2d649cbb318a36d930da8c10d8e5b19e3bde49f5
vovaf <vovafokyz@gmail.com>
2023-11-26 10:09:52 +0200

    Репозиторий под проект Tracker

--- src/main/java/ru/job4j/collection/UniqueText.java
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -5,16 +5,12 @@ import java.util.HashSet;
 public class UniqueText {
     public boolean isEquals(String originText, String duplicateText) {
         boolean rsl = true;
-
         String[] origin = originText.split(" ");
         String[] text = duplicateText.split(" ");
-
         HashSet<String> check = new HashSet<>();
-
         for (String word : origin) {
             check.add(word);
         }
-
         for (String word : text) {
             if (!check.contains(word)) {
                 rsl = false;
