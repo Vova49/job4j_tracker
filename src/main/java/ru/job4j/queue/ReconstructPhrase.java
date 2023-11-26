384003d223c72be0a98a43bf5a4a970ebf3185fa
vovaf <vovafokyz@gmail.com>
2023-11-26 15:07:34 +0200

    Репозиторий под проект Tracker

---
+++ src/main/java/ru/job4j/queue/ReconstructPhrase.java
@@ -0,0 +1,38 @@
+package ru.job4j.queue;
+
+import java.util.Deque;
+
+public class ReconstructPhrase {
+
+    private final Deque<Character> descendingElements;
+    private final Deque<Character> evenElements;
+
+    public ReconstructPhrase(Deque<Character> descendingElements, Deque<Character> evenElements) {
+        this.descendingElements = descendingElements;
+        this.evenElements = evenElements;
+    }
+
+    private String getEvenElements() {
+        StringBuilder result = new StringBuilder();
+        int index = 0;
+        for (Character character : evenElements) {
+            if (index % 2 == 0) {
+                result.append(character);
+            }
+            index++;
+        }
+        return result.toString();
+    }
+
+    private String getDescendingElements() {
+        StringBuilder result = new StringBuilder();
+        while (!descendingElements.isEmpty()) {
+            result.append(descendingElements.pollLast());
+        }
+        return result.toString();
+    }
+
+    public String getReconstructPhrase() {
+        return getEvenElements() + getDescendingElements();
+    }
+}
