776334263143545f11774576ea39ebd9bf80d7ab
vovaf <vovafokyz@gmail.com>
2023-11-27 09:54:33 +0200

    Репозиторий под проект Tracker

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -6,6 +6,7 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
         map.put("vovafokyz@gmail.com", "Ivanov Volodymyr Olegovich");
+        map.put("stepankorobko@gmail.com", "Ivan Stoimenov Geshev");
         map.put("stepankorobko@gmail.com", "Stepan Korobko Petrovich");
         for (String key : map.keySet()) {
             System.out.println("Email: " + key + ", ФИО: " + map.get(key));
