975dfcf747a2f3cf188946e2baf932edf6325836
vovaf <vovafokyz@gmail.com>
2023-12-04 11:08:16 +0200

    Репозиторий под проект Tracker

---
+++ src/test/java/ru/job4j/collection/JobTest.java
@@ -0,0 +1,51 @@
+package ru.job4j.collection;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Comparator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class JobTest {
+    @Test
+    public void whenComparatorAscByName() {
+        Comparator<Job> comparator = new JobAscByName();
+        int rsl = comparator.compare(new Job("Impl task", 0), new Job("Fix bug", 1));
+        assertThat(rsl).isGreaterThan(0);
+    }
+
+    @Test
+    public void whenComparatorAscByPriority() {
+        Comparator<Job> comparator = new JobAscByPriority();
+        int rsl = comparator.compare(new Job("Impl task", 0), new Job("Fix bug", 1));
+        assertThat(rsl).isLessThan(0);
+    }
+
+    @Test
+    public void whenComparatorDescByName() {
+        Comparator<Job> comparator = new JobDescByName();
+        int rsl = comparator.compare(new Job("Impl task", 0), new Job("Fix bug", 1));
+        assertThat(rsl).isLessThan(0);
+    }
+
+    @Test
+    public void whenComparatorDescByPriority() {
+        Comparator<Job> comparator = new JobDescByPriority();
+        int rsl = comparator.compare(new Job("Impl task", 0), new Job("Fix bug", 1));
+        assertThat(rsl).isGreaterThan(0);
+    }
+
+    @Test
+    public void whenComparatorByNameAndPriority() {
+        Comparator<Job> comparator = new JobAscByName().thenComparing(new JobAscByPriority());
+        int rsl = comparator.compare(new Job("Fix bug", 1), new Job("Impl task", 0));
+        assertThat(rsl).isLessThan(0);
+    }
+
+    @Test
+    public void whenComparatorByDescNameAndDescPriority() {
+        Comparator<Job> comparator = new JobDescByName().thenComparing(new JobDescByPriority());
+        int rsl = comparator.compare(new Job("Fix bug", 1), new Job("Impl task", 0));
+        assertThat(rsl).isGreaterThan(0);
+    }
+}
